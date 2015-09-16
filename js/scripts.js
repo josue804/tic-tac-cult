811b8aaad50820df301f43c1a3f0164722b025d3
Kellen Kolbeck & Vaughn Dice <student@epicodus.com>
2015-09-16 10:04:17 -0700

    add Board object and find method

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,23 @@ Space.prototype.xCoord = function() {
 Space.prototype.yCoord = function() {
   return this.y;
 }
+
+function Board() {
+  this.spaces = [];
+
+  for(var i = 0; i < 3; i++) {
+    for(var j = 0; j < 3; j++) {
+      this.spaces.push(new Space(i, j));
+    }
+  }
+}
+
+Board.prototype.find = function(x, y) {
+  var foundSpace = {};
+  this.spaces.forEach(function(space) {
+    if (space.xCoord() === x && space.yCoord() === y) {
+      foundSpace = space;
+    }
+  });
+  return foundSpace;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -6,12 +6,20 @@ describe('Player', function(){
 });
 
 describe("Space", function() {
-  it("returns the players mark", function() {
+  it("returns space\'s y coordinate", function() {
     var testSpace = new Space(1,2);
     expect(testSpace.yCoord()).to.equal(2)
   });
-  it("returns the players mark", function() {
+  it("returns space\'s x coordinate", function() {
     var testSpace = new Space(1,2);
     expect(testSpace.xCoord()).to.equal(1)
+  });
+});
+
+describe("Board", function() {
+  it('creates 9 spaces when initialized', function() {
+    var testSpace = new Space(1, 2);
+    var testBoard = new Board();
+    expect(testBoard.find(1, 2)).to.eql(testSpace);
   });
 });
